88c790f46ab49f5bd7baae2333e2dcbe2eab92d5
zhaodongyue <zdykiller@126.com>
2024-05-02 18:08:00 +0800

    从配置中读取notify url，格式化代码

--- WechatTool/WechatMpNotifyChecker.user.js
+++ WechatTool/WechatMpNotifyChecker.user.js
@@ -9,9 +9,10 @@
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // 获取第一个notify_id
@@ -21,36 +22,40 @@
     }
 
     // 发送webhook通知
-    function sendWebhookNotification(notifyId, title, content) {
-        GM_xmlhttpRequest({
-            method: 'POST',
-            url: '', // 将此替换为你的webhook URL
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            data: JSON.stringify(
-                {"msg_type":"text","content":{"text":`notify_id:${notifyId}\n${title}\n${content}`}}
-            ),
-            onload: function(response) {
-                GM_setValue('lastSendSuccessId', notifyId);
-                console.log('webhook通知发送成功:', response);
-            },
-            onerror: function(error) {
-                GM_setValue('lastSendFailId', notifyId);
-                console.error('webhook通知发送失败:', error);
-            }
+    async function sendWebhookNotification(notifyId, title, content) {
+        return new Promise((resolve, reject) => {
+            // 将此替换为你的webhook URL
+            let url = GM_getValue("notifyUrl");
+            GM_xmlhttpRequest({
+                method: "POST",
+                url: url,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                data: JSON.stringify(
+                    {"msg_type": "text", "content": {"text": `notify_id:${notifyId}\n${title}\n${content}`}}
+                ),
+                onload: function (response) {
+                    GM_setValue("lastSendSuccessId", notifyId);
+                    console.log('webhook通知发送成功:', response);
+                    resolve(response);
+                },
+                onerror: function (error) {
+                    GM_setValue("lastSendFailId", notifyId);
+                    console.error('webhook通知发送失败:', error);
+                    reject(error);
+                }
+            });
         });
     }
 
     // 检查notify_id是否发生变化
     function checkForUpdates() {
-        debugger;
         let firstNotify = getFirstNotify();
-
-        if(!firstNotify){
+        if (!firstNotify) {
             console.log("not found notify");
-            const sendFailedMsg = GM_getValue('sendFailedMsg');
-            if(sendFailedMsg !== "send"){
+            const sendFailedMsg = GM_getValue("sendFailedMsg");
+            if (sendFailedMsg !== "send") {
                 let unknownId = "unknownId"
                 sendWebhookNotification(unknownId, "没找到mp页面中的通知", "请查看是否是账号登录过期");
                 GM_setValue('sendFailedMsg', "send");
@@ -58,21 +63,33 @@
             return;
         }
 
-        const previousNotifyId = GM_getValue('previousNotifyId');
+        const previousNotifyId = GM_getValue("previousNotifyId");
         const currentNotifyId = firstNotify.getAttribute("notify_id");
         if (currentNotifyId && currentNotifyId !== previousNotifyId) {
-            console.log('检测到新的通知内容');
+            console.log("检测到新的通知内容");
             // 在此处添加处理新通知内容的逻辑
-            GM_setValue('previousNotifyId', currentNotifyId);
+            GM_setValue("previousNotifyId", currentNotifyId);
             let title = firstNotify.querySelector("[class='notice_title']").innerText;
             let notifyContent = firstNotify.querySelector("[class='dn']").innerText;
             sendWebhookNotification(currentNotifyId, title, notifyContent);
         }
     }
 
+    function testSend() {
+        sendWebhookNotification("测试", "测试消息", "发送测试通知").then((resp) => {
+            alert(`${resp.status}`)
+        }).catch((err) => {
+            alert(`${err.error} 请在存储中配置 notifyUrl={你想发送的webhook}`)
+        })
+    }
+
     // 页面加载完毕后检查更新并开始定时刷新
-    window.addEventListener('load', function () {
-        setTimeout(()=>{
+    window.addEventListener("load", function () {
+        GM_registerMenuCommand("测试发送通知", function (event) {
+            testSend()
+        });
+
+        setTimeout(() => {
             checkForUpdates();
         }, 10 * 1000);
         setInterval(function () {
